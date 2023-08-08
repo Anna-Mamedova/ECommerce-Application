29ba832307d73cbba685cfe09c7a9504fe2cda2e
Angelina Liapin <angelina.liapin@gmail.com>
2023-08-08 18:03:03 +0300

    feat: implement log in page

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,9 @@ import App from './components/App';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootElement);
-const num1 = 5;
-const num2 = 10;
 
 root.render(
   <StrictMode>
-    <App num1={num1} num2={num2} />;
+    <App />
   </StrictMode>
 );
